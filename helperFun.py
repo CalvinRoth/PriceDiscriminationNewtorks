951f2a242f8ea37f5b1cf762250842eb7337055f
CalvinRoth <rothx195@umn.edu>
2021-11-12 12:35:14 -0600

    Major commit. Lots of refactoring and added parallel processing

--- helperFun.py
+++ helperFun.py
@@ -10,7 +10,7 @@ from scipy.stats import chi2_contingency
 # #  Prelimaries 
 # First we implement the powerIteration method in order to quickly estimate the largest eigenvalue
 
-def powerIteration(A, iters=500):
+def powerIteration(A, iters=20):
     n = A.shape[0]
     b = np.random.randn(n,)
     b = b / lin.norm(b, ord=2)
@@ -23,6 +23,10 @@ def snorm(A):
     b = powerIteration(A.T @ A)
     return np.sqrt((b.T @ A.T @ A @ b))
 
+def specNorm(A):
+    return  np.sqrt(scipy.sparse.linalg.eigs(A.T @ A, k=1, which="LM",return_eigenvectors=False, tol=1e-5)[0])
+
+
 #Graph Generators
 def makeERGraph(n,p):
     """ Generates Random Erdos-Renyi Graphs with n vertices and link probability p
@@ -31,15 +35,16 @@ def makeERGraph(n,p):
         Adjacency graph of matrix and the networkx DiGraph object
     """
     G = nx.generators.fast_gnp_random_graph(n,p, directed=True)
-    return nx.adj_matrix(G), G
+    return nx.to_scipy_sparse_matrix(G, dtype="d"), G
 
 
 def makeSimilarGraph(G):
 
     sequence_in = [d for n,d in G.in_degree()]
     sequence_out = [d for n,d in G.out_degree()]
-    return nx.adj_matrix(
-        nx.directed_configuration_model(sequence_in, sequence_out, create_using=nx.DiGraph)
+    return nx.to_scipy_sparse_matrix(
+        nx.directed_configuration_model(sequence_in, sequence_out, create_using=nx.DiGraph),
+        dtype="d"
     )
 
 
@@ -47,7 +52,7 @@ def makeSimilarGraph(G):
 # Profits 
 def profitUniform(A, rho):
     n = A.shape[0]
-    alpha = 2*rho/snorm(A + A.T)
+    alpha = 2*rho/specNorm(A + A.T)
     temp = I - alpha* A
     temp = lin.inv(temp)
     return (ones.T @ temp @ ones)[0,0]
@@ -55,7 +60,7 @@ def profitUniform(A, rho):
 
 def profitDiscrim(A,rho):
     n = A.shape[0]
-    alpha = rho / snorm(A + A.T)
+    alpha = rho / specNorm(A + A.T)
     inner = I - alpha*(A + A.T)
     return (ones.T @ lin.inv(inner) @ ones)[0,0]
 
@@ -67,7 +72,7 @@ def priceVector(A, rho,a,c ):
     ident = np.eye(n,n)
     ones = np.ones((n,1))
     ApA = A + A.T
-    eig = snorm(ApA)
+    eig = specNorm(ApA)
     alpha = rho / eig
     central = lin.inv(ident - alpha*ApA) @ ones
     dif = A - A.T
@@ -79,7 +84,7 @@ def applyPriceVector(A, v, rho,  a, c):
     ident = np.eye(n,n)
     ones = np.ones((n,1))
     ApA = A + A.T
-    consumption = (2*rho/snorm(ApA)) * A
+    consumption = (2*rho/specNorm(ApA)) * A
     consumption = 0.5 * lin.inv(ident - consumption)
     consumption = consumption @ (a*ones - v)
     return ((v - c*ones).T @ consumption)[0,0]
@@ -106,6 +111,18 @@ def averageGapReverse(A,test, rho, a,c):
 
 def getGaps(n, p, rho, a, c, i, results, n_trials):
     A, G = makeERGraph(n,p)
+    print(n, p)
     results[i] = np.average( [getGap(A, makeSimilarGraph(G), rho, a, c) for j in range(n_trials)])
     print("Done,", i)
     return i
+
+
+def getAverageGap(n, p, rho, a, c, i,  results, n_trials):
+    A, G = makeERGraph(n,p)
+    n = A.shape[0]
+    trueProfit = applyPriceVector(A, priceVector(A, rho, a,c), rho, a, c)
+    averageV = priceVector(makeSimilarGraph(G), rho, a,c)
+    for i in range(n_trials-1):
+        averageV += priceVector(makeSimilarGraph(G), rho, a, c)
+    profit = applyPriceVector(A, (1/n_trials) * trueProfit, rho, a, c)
+    return trueProfit-profit
--- script.py
+++ script.py
@@ -2,46 +2,53 @@ from helperFun import *
 import multiprocessing as mp
 import time
 
+if __name__ == '__main__':
+        tik = time.time()
+        # Paramters
+        steps =150
+        n_range = [i*200 for i in range(1,steps+1)]
+        n = 1000
+        p0 = 1.3/n
+        p1 = 2* np.log(n)/n
+        p = 1.3/n
+        delta = (p1-p0)/steps
+        p_range = [p0+i*delta for i in range(steps)]
+        rho = 0.9
+        a = 4
+        c = 2
+        n_trials = 20
 
-tik = time.time()
-# Paramters
-n = 1200
-p0 = 1/n
-p1 = 2* np.log(n)/n
-steps = 10
-delta = (p1-p0)/steps
-Ps = [p0+i*delta for i in range(steps)]
-rho = 0.9
-a = 4
-c = 2
-n_trials = 20
+        multiprocess = False
+        max_threads = 8
 
+        if(multiprocess):
+                manager = mp.Queue(maxsize=max_threads)
+                results = mp.Array("f", range(steps))
+                jobs = []
+        else:
+                results = [0 for i in range(steps)]
 
-i = 0 
-#pool = mp.Pool(Process=4)
-#result = pool.apply_async(getGaps, (n, Ps[p], rho, a, c, p, average_gaps, n_trials))
-#result.ready()
-manager = mp.Queue()
-average_gaps = manager.dict()
+        currentTest = getGaps
 
-jobs = []
-for p in range(steps):
-	job = mp.Process(target=getGaps, args= (n, Ps[p], rho, a, c, p, average_gaps, n_trials))
-	jobs.append(job)
-#	job.start()
-	#getGets(trueAdj, trueG, rho, a, c, p, average_gaps, n_trials)
-	#print(i)
-	#average_gaps.append(np.average([
-	#	getGap(trueAdj, makeSimilarGraph(trueG), rho, a, c) 
- 	#	for i in range(n_trials)
- 	#	]))
+        print("N vertices", "probability of links")
+        for i in range(steps):
+                currentParams = (n_range[i], p, rho, a, c, i, results, n_trials)
+                if(multiprocess):
+                        job = mp.Process(target=currentTest, args=currentParams)
+                        jobs.append(job)
+                        job.start()
+                else:
+                        currentTest(*currentParams)
 
-#for job in jobs:
-#	job.join()
+        if(multiprocess):
+                for job in jobs:
+                        job.join()
 
-tok = time.time()
-print(tok-tik)
+        tok = time.time()
+        print(tok-tik)
 
-print(average_gaps)
-plt.title("Gap with different p")
-plt.plot(Ps, average_gaps)
+        print(list(results))
+        #plt.show()
+        print(type(steps), type(results))
+        plt.plot(n_range, results)
+        plt.show()
