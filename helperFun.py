cbf15194bb2d1943ec1712e6e84d0a1a9087cd6a
CalvinRoth <rothx195@umn.edu>
2021-12-16 14:41:31 -0600

    Added new files to test more things

--- helperFun.py
+++ helperFun.py
@@ -2,146 +2,163 @@ import numpy as np
 import numpy.linalg as lin
 import networkx as nx
 import scipy
+import scipy.sparse.linalg as slin
 import matplotlib.pyplot as plt
-from scipy.stats import chi2_contingency 
-import multiprocessing as mp
 
 
-# #  Prelimaries 
+# #  Preliminaries
 # First we implement the powerIteration method in order to quickly estimate the largest eigenvalue
 
 def powerIteration(A, iters=20):
     n = A.shape[0]
-    b = np.random.randn(n,)
+    b = np.random.randn(n, )
     b = b / lin.norm(b, ord=2)
     for i in range(iters):
         bnew = A @ b
-        b = bnew / lin.norm(bnew, ord = 2)
+        b = bnew / lin.norm(bnew, ord=2)
     return b
+
 
 def snorm(A):
     b = powerIteration(A.T @ A)
     return np.sqrt((b.T @ A.T @ A @ b))
 
+
 def specNorm(A):
-    return  np.sqrt(scipy.sparse.linalg.eigs(A.T @ A, k=1, which="LM",return_eigenvectors=False, tol=1e-5)[0])
+    return np.sqrt(slin.eigs(A.T @ A, k=1, which="LM", return_eigenvectors=False, tol=1e-5)[0])
 
 
-#Graph Generators
-def makeERGraph(n,p):
+# Graph Generators
+def makeERGraph(n, p):
     """ Generates Random Erdos-Renyi Graphs with n vertices and link probability p
-        Returns 
         ------
-        Adjacency graph of matrix and the networkx DiGraph object
+        return Adjacency graph of matrix and the networkx DiGraph object
     """
-    G = nx.generators.fast_gnp_random_graph(n,p, directed=True)
+    G = nx.generators.fast_gnp_random_graph(n, p, directed=True)
+    # sortG = sorted(G.in_degree, key=lambda x: x[1], reverse=True)
     return nx.to_scipy_sparse_matrix(G, dtype="d"), G
 
 
 def makeSimilarGraph(G):
-
-    sequence_in = [d for n,d in G.in_degree()]
-    sequence_out = [d for n,d in G.out_degree()]
+    """ Generates the new graph with the same in/out degree as the orginal
+        -------
+        Return adj. matrix of graph
+    """
+    sequence_in = [d for n, d in G.in_degree()]
+    sequence_out = [d for n, d in G.out_degree()]
     return nx.to_scipy_sparse_matrix(
         nx.directed_configuration_model(sequence_in, sequence_out, create_using=nx.DiGraph),
         dtype="d"
     )
 
 
-
 # Profits 
-def profitUniform(A, rho):
+def profitUniform(A, rho, a, c):
     n = A.shape[0]
-    alpha = 2*rho/specNorm(A + A.T)
-    temp = I - alpha* A
+    ones = np.ones((n, 1))
+    alpha = 2 * rho / specNorm(A + A.T)
+    temp = np.eye(n, n) - alpha * A
     temp = lin.inv(temp)
-    return (ones.T @ temp @ ones)[0,0]
+    return (((a - c) * (a - c)) / 8) * (ones.T @ temp @ ones)[0, 0]
 
 
-def profitDiscrim(A,rho):
-    n = A.shape[0]
-    alpha = rho / specNorm(A + A.T)
-    inner = I - alpha*(A + A.T)
-    return (ones.T @ lin.inv(inner) @ ones)[0,0]
+# Optimal profit under price discrim
+def profitDiscrim(A, n, p, a, c, rho):
+    # A and C should only impact graph up to scaling
+    one = np.ones((n, 1))
+    weight = rho / specNorm(A + A.T)
+    t1 = lin.inv(np.eye(n, n) - (weight * (A + A.T)))
+    total = one.T @ t1 @ one
+    total = ((a - c) * (a - c) / 8) * total
+    return np.real(total[0, 0])
 
 
 # Price Vectors
-def priceVector(A, rho,a,c ):
+def priceVector(A, rho, a, c):
+    """ Get the price vector of graph A """
     n = A.shape[0]
-    
-    ident = np.eye(n,n)
-    ones = np.ones((n,1))
+
+    ident = np.eye(n, n)
+    ones = np.ones((n, 1))
     ApA = A + A.T
-    eig = specNorm(ApA)
+    eig = np.real(specNorm(ApA))
     alpha = rho / eig
-    central = lin.inv(ident - alpha*ApA) @ ones
+    central = lin.inv(ident - alpha * ApA)
+    central = central @ ones
     dif = A - A.T
-    return ((a+c)/2)*ones +  ((a-c)*alpha/2) * dif @ central
+    return ((a + c) / 2) * ones + ((a - c) * alpha / 2) * (dif @ central)
 
 
-def applyPriceVector(A, v, rho,  a, c):
+def applyPriceVector(A, v, rho, a, c):
+    """ Apply price vector to find the profits of A"""
     n = A.shape[0]
-    ident = np.eye(n,n)
-    ones = np.ones((n,1))
+    ident = np.eye(n, n)
+    ones = np.ones((n, 1))
     ApA = A + A.T
-    consumption = (2*rho/specNorm(ApA)) * A
-    consumption = 0.5 * lin.inv(ident - consumption)
-    consumption = consumption @ (a*ones - v)
-    return ((v - c*ones).T @ consumption)[0,0]
-
+    spN = np.real(specNorm(ApA))  # Sometimes scipy return x+0i, this is to discard warning
+    consumption = (2 * rho / spN) * A
+    consumption = ident - consumption;
+    consumption = 0.5 * lin.inv(consumption)
+    consumption = consumption @ ((a * ones) - v)
+    #if (np.min(consumption) < 0):
+        # My understanding is this should literally never print
+     #   print(spN, np.min(v), np.min(A))
+    return (v - c * ones).T @ consumption
 
 
 # Gaps applying price vector of G to guesses
-def getGap(A, test, rho, a, c):
+def applyTrueVector(A, test, rho, a, c):
+    """ Apply optimal profit price vector A to test graph test and A. Return pair of profits"""
     optimalVector = priceVector(A, rho, a, c)
-    profitAtGuess = applyPriceVector(test, optimalVector, rho, a,c)
-    trueProfit = applyPriceVector(A, optimalVector, rho, a,c)
-    return trueProfit - profitAtGuess
-
+    profitAtGuess = applyPriceVector(test, optimalVector, rho, a, c)
+    trueProfit = applyPriceVector(A, optimalVector, rho, a, c)
+    return trueProfit, profitAtGuess
 
 
 # Gaps applying price vector of guesses to true graph G
-def getGapRev(A,test, rho, a,c):
-    optimalVector = priceVector(test, rho, a,c)
+def getGapRev(A, test, rho, a, c):
+    """ Apply optimal profit price vector guess graph to test graph test and A. Return pair of profits"""
+    optimalVector = priceVector(test, rho, a, c)
     profitWithGuessV = applyPriceVector(A, optimalVector, rho, a, c)
-    trueProfit = applyPriceVector(A, priceVector(A, rho,a,c), rho, a, c)
-    return trueProfit - profitWithGuessV
-### AHHHHHHH
+    trueProfit = applyPriceVector(A, priceVector(A, rho, a, c), rho, a, c)
+    return trueProfit, profitWithGuessV
+
 
 # Applying the true optimal profit vector to guesses
 # Currently not using because it seems backwards of what I want
 def getGaps(n, p, rho, a, c, i, results, n_trials):
-    A, G = makeERGraph(n,p)
-    results[i] = np.average( [getGap(A, makeSimilarGraph(G), rho, a, c) for j in range(n_trials)])
+    A, G = makeERGraph(n, p)
+    results[i] = np.average([getGap(A, makeSimilarGraph(G), rho, a, c) for j in range(n_trials)])
     return i
 
+
 # Apply the price vector that each guess produces to the true graph and take average.
-def getGapsReverse(n,p,rho, a,c, i, results, n_trials):
-    A,G = makeERGraph(n,p)
-    results[i] = np.average( [getGapRev(A, makeSimilarGraph(G), rho, a,c) for j in range(n_trials)])
+def getGapsReverse(n, p, rho, a, c, i, results, n_trials):
+    A, G = makeERGraph(n, p)
+    results[i] = np.average([getGapRev(A, makeSimilarGraph(G), rho, a, c) for j in range(n_trials)])
     return i
+
 
 # Here we apply the average optimal price vector of the guesses to the true graph G
 # I get a warning about discarding complex values, values should never complex for this problem
 # and when I check they are all +0i so ?
 
 
-def getAverageGap(n, p, rho, a, c, i,  results, n_trials):
-    A, G = makeERGraph(n,p)
+def getAverageGap(n, p, rho, a, c, i, results, n_trials):
+    A, G = makeERGraph(n, p)
     n = A.shape[0]
-    trueProfit = applyPriceVector(A, priceVector(A, rho, a,c), rho, a, c)
+    trueProfit = applyPriceVector(A, priceVector(A, rho, a, c), rho, a, c)
     # the average vector initilized with sample size of 1
-    averageV = priceVector(makeSimilarGraph(G), rho, a,c)
+    averageV = priceVector(makeSimilarGraph(G), rho, a, c)
     # And another n_trials-1 trials
-    for j in range(n_trials-1):
+    for j in range(n_trials - 1):
         averageV += priceVector(makeSimilarGraph(G), rho, a, c)
-    averageV /= n_trials # Scaling
+    averageV /= n_trials  # Scaling
     profit = applyPriceVector(A, averageV, rho, a, c)
     results[i] = np.real(trueProfit - profit)
 
 
-
-def fractionalGap(n,p, rho, a, c, i, results, n_trials):
+def fractionalGap(n, p, rho, a, c, i, results, n_trials):
     A, G = makeERGraph(n, p)
     n = A.shape[0]
     trueProfit = np.real(applyPriceVector(A, priceVector(A, rho, a, c), rho, a, c))
@@ -152,9 +169,51 @@ def fractionalGap(n,p, rho, a, c, i, results, n_trials):
         averageV += priceVector(makeSimilarGraph(G), rho, a, c)
     averageV /= n_trials  # Scaling
     profit = np.real(applyPriceVector(A, averageV, rho, a, c))
-    results[i] = trueProfit/profit
+    results[i] = trueProfit / profit
+
 
 # Get the variance of the price vectors
-def varianceVector(n, p, rho, a, c, i, results, n_trails):
-    return np.var([getGapRev(A, makeSimilarGraph(G), rho, a,c) for j in range(n_trials)])
+def varianceVector(n, p, rho, a, c, n_trials):
+    A, G = makeERGraph(n, p)
+    generated_vs = np.zeros((n, n_trials))
+    true_vector = priceVector(A, rho, a, c)
+    for i in range(n_trials):
+        v = true_vector - priceVector(makeSimilarGraph(G), rho, a, c)
+        v.resize((n,))
+        generated_vs[:, i] = v
+    return np.var(generated_vs, axis=1)
 
+
+# How much does the profit change when we change the ith coordinate of price vector
+# Change each percent wise. Test +/- percent
+def robustNess(n, p, chaos, rho, a, c):
+    A, G = makeERGraph(n, p)
+    true_vector = priceVector(A, rho, a, c)
+    true_profit = applyPriceVector(A, true_vector, rho, a, c)
+    results = np.zeros(n)
+    count_range = [i for i in range(n)]
+    inD = [d[1] for d in G.in_degree]
+    outD = [d[1] for d in G.out_degree]
+
+    plt.plot(inD)
+    plt.plot(outD)
+    plt.show()
+    """for i in range(n):
+        increase_v = true_vector
+        decrease_v = true_vector
+        increase_v[i] += chaos * increase_v[i];
+        decrease_v[i] -= chaos * decrease_v[i];
+        profitI = true_profit - applyPriceVector(A, increase_v, rho, a,c);
+        profitD = true_profit - applyPriceVector(A, decrease_v, rho, a,c);
+        results[i] = max(profitI, profitD)"""
+    return results
+
+
+def fractionalRegret(A, v, n, p, rho, a, c):
+    """ Fractional regret relative to price vector v """
+    discrim = profitDiscrim(A, n, p, a, c, rho)  # Optimal profit
+    ds2 = applyPriceVector(A, priceVector(A,rho,a,c),rho, a, c)
+    if(np.abs(discrim - ds2) > 0.00001 ):
+        print("Hmmmm")
+    appliedProf = np.real(applyPriceVector(A, v, rho, a, c))  # Profit at v
+    return 1 - (appliedProf / discrim)
--- script.py
+++ script.py
@@ -1,14 +1,17 @@
 from helperFun import *
+import tests
 import multiprocessing as mp
 import pandas as pd
 from stopwatch import Stopwatch
+from scipy import interpolate
+
 
 if __name__ == '__main__':
         timer = Stopwatch();
         timer.start()
         # Paramters
-        steps =10
-        n = 2500
+        steps =14
+        n = 1500
         n_delta = (n-400)/steps
         n_range = [ int(400+ i*n_delta) for i in range(0,steps)]
         p0 = 0.5/n
@@ -20,13 +23,15 @@ if __name__ == '__main__':
         rho = 0.9
         a = 4
         c = 2
-        n_trials = 20
+        n_trials = 50
         n_trials_range = np.linspace(5,100, steps, dtype=int)
         outer_steps = 5
+        """
         results = [ [0 for i in range(steps)] for j in range(outer_steps)]
         results1 = [0 for i in range(steps)]
         results2 = [0 for i in range(steps)]
         results3 = [0 for i in range(steps)]
+
         #results4 = [0 for i in range(steps)]
         # Test1 is apply the price vector of true graph to guessed graph and take average
         # Test2 is apply price vector of guess graph to true graph and take average
@@ -42,10 +47,10 @@ if __name__ == '__main__':
         jobs = [];
         for i in range(steps):
                 for j in range(outer_steps):
-                        currentParam4 = (n, p, rho, a, c, i, results[j], n_trials_range[i])
-                        getAverageGap(*currentParam4)
-                        #currentParam5 = (n, p_range[i], rho, a, c, i, results[j], n_trials)
-                        #fractionalGap(*currentParam5)
+                        #currentParam4 = (n, p, rho, a, c, i, results[j], n_trials_range[i])
+                        #getAverageGap(*currentParam4)
+                        currentParam5 = (n, p_range[i], rho, a, c, i, results[j], n_trials)
+                        fractionalGap(*currentParam5)
                 #currentParams1 = (n, p_range[i], rho, a, c, i, results1, n_trials)
                 #currentParams2 = (n_range[i], p_foo(i), rho, a, c, i, results2, n_trials)
                 #currentParams3 = (n_range[i], p_foo(i), rho, a, c, i, results3, n_trials)
@@ -59,7 +64,6 @@ if __name__ == '__main__':
 
         #for job in jobs:
         #        job.join()
-
         timer.stop()
         full_results = np.zeros((steps, outer_steps+1))
         full_results[:,0] = np.array(n_trials_range)
@@ -76,3 +80,61 @@ if __name__ == '__main__':
         plt.show()
         #tabled = pd.DataFrame(full_results, columns = ["p value", "Test 1 ", "Test 2", "Test 3"])
         #tabled.to_csv("n_trials1_3_n.csv")
+        """
+
+        #resultsVar = varianceVector(n, p, rho, a, c, n_trials)
+        #robustRes = robustNess(n, p, 0.05, rho,  a,c)
+        #count_range = [i for i in range(n)]
+
+
+        #results = tests.nTrialstest(n,p, n_trials_range, rho, a,c)
+        #results1 = tests.sameparamProfitsVarTest(n,p_range, n_trials, rho, a, c)
+        #results2 = tests.sameseqProfitsVarTest(n,p_range, n_trials, rho, a,c)
+        #print(np.var(results1))
+        #print(np.var(results2))
+        #fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
+        #axs[0].plot(p_range, results1)
+        #axs[0].plot(p_range, results2)
+
+        """results0 = tests.sameseqcomparePrices(n,1/n,n_trials, rho, a, c)
+        results1 = tests.sameseqcomparePrices(n, 2/n, n_trials, rho, a,c)
+        results2 = tests.sameseqcomparePrices(n, np.log(n)/n, n_trials, rho, a,c)
+        var1 = np.var(results0, axis=1)
+        var2 = np.var(results1, axis=1)
+        var3 = np.var(results2, axis=1)
+
+        # Filter out 0 var nodes for plotting
+        var1 = [i for i in var1 if i!=0]
+        var2 = [i for i in var2 if i!=0]
+        var3 = [i for i in var3 if i!=0]
+        hist1 = np.histogram(var1, bins=len(var1))
+        hist2 = np.histogram(var2, bins=len(var2))
+        hist3 = np.histogram(var3, bins=len(var3))
+
+        plt.hist(var1, bins=n, label="1/n")
+        plt.hist(var2, bins=n, label="2/n")
+        plt.hist(var3, bins=n, label="log(n)/n")
+        plt.legend()
+        plt.title("Effect of p on node prices")"""
+
+
+        n = 100
+        n_trials = 30
+        steps = 30
+        counting = [i for i in range(n)]
+        p = np.log(n)/n
+        p0 = 1.1/n
+        p1 = np.log(n)/n
+        delta = (p1-p0)/steps
+        p_space = [p0+(delta*i) for i in range(steps)]
+        resSeq, resPar = tests.fractionalRegretOVerP(n,p0, p1, n_trials, steps, rho, a,c)
+        plt.title("Fractional regret as a function of p")
+        plt.xlabel("p")
+        plt.ylabel("regret")
+        plt.plot(p_space, resSeq, label="SameSeq")
+        plt.plot(p_space, resPar, label="SameParam")
+        plt.legend()
+        plt.show()
+
+        print(tests.sanityCheck(n,p,rho, a,c))
+
